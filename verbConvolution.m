2d910a5d04997eda899757643d0a11c86d8b7dec
willbanks27 <134657048+willbanks27@users.noreply.github.com>
2023-11-13 19:50:51 -0500

    Add files via upload

--- verbConvolution.m
+++ verbConvolution.m
@@ -1,97 +1,65 @@
-%%% reverb emulation via convolution 
-%%% Will Banks - EE Undergrad @UKY
 
-%%% read inputs & convert to mono (if needed)
 
-    %%% vox signal
+
+%%% goal: to have the length of 'in' equal the length of 'verb'
+
+%%% below is code that padds the input signal to have the same index length
+
+%%% define inputs
+
+[in, fs] = audioread('ghosts.wav'); % input signal defined
+    in = in(:,1); % converts signal to mono, if needed
+
+[verb, ~] = audioread('warehouse.wav'); % reverb response signal defined
+    verb = verb(:,1); % converts signal to mono, if needed
+
+%%% inputs are columns. zero pad command requires rows 
+
+    %%% convert signals to rows
     
-        [input, fs] = audioread('smash_hit.wav');
-            input = input(:,1); % converts to mono 
+        in_row = in.';
+        verb_row = verb.';
 
-            tAxis_in = (0:length(input)-1)/fs; %defines time axis and converts index units to seconds      
+    %%% zero pad the input signal
     
-    %%% system response signal
-    
-        [verb, ~] = audioread('warehouse2.wav');
-            verb = verb(:,1); % converts to mono
+        zeros_needed = length(verb) - length(in_row);
+        
+        in_padded_row = [in_row, zeros(1,zeros_needed)];
 
-            tAxis_verb = (0:length(input)-1)/fs; %defines time axis and converts index units to seconds    
+%%% convert the padded signal from a row to column
 
-%%% define various sampling stuff for PSD
+    in_padded = in_padded_row.';
 
-    nfft = fs;                          % number of nfft points equals sampling rate
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%% convolution of the input (in_padded) and response (verb)
+
+%%% defining sampling stuff
+
+    nfft = length(in);                  % nfft points equal to length of input
     window = fs/105;                    % window length value defined
     nfftPSD = window.*8;                % number of FFT points proportional to window length
     wTape = hamming(window);            % ham that window
     olap = floor(window/2);             % overlap proportional to half of window length
 
-%%% identity: y(t) = x(t)*h(t) --> Y(z) = X(z)H(z)
+%%% Z transforms, XzT-> input, HzT->response, YzT->output
 
-    Xz = fft(input, nfft); % input Z transform
-        XzdB = 20.*log10(abs(Xz)); % dB conversion for plot
+    XzT = fft(in_padded, nfft);
+    HzT = fft(verb, nfft);
     
-    Hz = fft(verb, nfft); % response Z transform
-        HzdB = 20.*log10(abs(Hz)); % dB conversion for plot
-    
-    Yz = Xz.*Hz; % Z transform convolution property
-        YzdB = 20.*log10(abs(Yz)); % dB conversion for plot
+    YzT = XzT.*HzT;
 
-%%% convert output into real domain
+%%% inverse Z transforms, x-> input, h->response, y->output
 
-    y = ifft(Yz, nfft); % inverse fft with nfft = nfft;
+    y = ifft(YzT, nfft);
 
-%%% PSD definitions
+%%% mixer    
 
-    [PSD_in, fAxis] = pwelch(input, window, olap, nfftPSD, fs);         % defines pwelch for noise floor
-        PSD_in2dB = 20.*log10(abs(PSD_in));                             % converts to decibel scale
-    
-    [PSD_resp, fAxis_resp] = pwelch(verb, window, olap, nfft, fs);      % defines pwelch for resting heartrate
-        PSD_resp2dB = 20.*log10(abs(PSD_resp));                         % converts to decibel scale
+    A = .05;
+    B = 1;
+    mix = A.*y + B.*in;
 
-    [PSD_out, fAxis_out] = pwelch(YzdB, window, olap, nfft, fs);        %defines pwelch for noise floor
-        PSD_out2dB = 20.*log10(abs(PSD_out));                           %converts to decibel scale
-
-%%% plots plots plots
-
-    tiledlayout(2,1);
-        
-        nexttile
-        
-            plot(fAxis, PSD_in2dB);
-                
-            hold on
-            
-            plot(fAxis_resp, PSD_resp2dB)
-            
-                set(gca, 'XScale', 'log')
-                title('Input Signal (Blue) Against Response Signal (Orange)')
-                xlabel('Frequency (Hz)')
-                ylabel('Gain (dB)')
-            
-                xlim([20 20e3])
-                ylim([-222 -80])
-        
-        nexttile
-        
-            plot(tAxis_in, input);
-                title('Input Signal')
-                xlabel('Amplitude')
-                ylabel('Seconds')
-
-            hold on
-            
-            plot(tAxis_verb, verb);
-
-            title('Input vs. Response Signal')
-                xlabel('Seconds')
-                ylabel('Amplitude')
-                
-            
-                xlim([0 1.05])
-
-%%% play output signal
-
-soundsc(y,fs)
+soundsc(mix,fs)
 
 
 
@@ -111,5 +79,10 @@ soundsc(y,fs)
 
 
 
+
+
+
+
+%
 
 
